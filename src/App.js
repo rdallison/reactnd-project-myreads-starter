3e4f577b72a5054bc08b4307e80ce483ba461a32
Ray <rayshawndallison@gmail.com>
2021-03-14 00:17:54 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import {Route} from 'react-router-dom'
+import ListBooks from './ListBooks'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-class BooksApp extends React.Component {
+class BooksApp extends Component {
   state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
@@ -16,28 +19,7 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
+        <Route exact path="/">
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -194,10 +176,13 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+              <Link to="/search"><button className="open-search">Add Book</button> </Link>
             </div>
           </div>
-        )}
+          </Route>
+        
+
+        <Route exact path="/search" component={ListBooks}></Route>
       </div>
     )
   }
---
+++ src/ListBooks.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import {get, getAll, search}  from './BooksAPI'
+
+class ListBooks extends Component{
+
+    state = {
+        query: "",
+        searchedBooks: [],
+    };
+
+
+    updateQuery = (query) => {
+      this.setState({
+          query: query.trim()
+      })   
+      search(this.state.query)
+      .then(data => (
+        this.setState(currentState => {
+          currentState.concat(data)
+        })
+      ))
+      
+      }
+      
+
+   
+
+    render(){
+
+      const {query, searchedBooks} = this.state;
+
+        return(
+            <div className="search-books">
+              {console.log(searchedBooks)}
+
+            <div className="search-books-bar">
+              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" placeholder="Search by title or author" value = {query}
+                    onChange= {(event) => this.updateQuery(event.target.value)}/>
+
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid">
+              
+              </ol>
+            </div>
+          </div>
+        );
+    }
+
+}
+
+export default ListBooks;
