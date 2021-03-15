8d6208bbc59abacb22606c834e9e9235cc399234
Ray <rayshawndallison@gmail.com>
2021-03-14 23:01:38 -0400

    third commit

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -39,7 +39,9 @@ class ListBooks extends Component{
 
         return(
             <div className="search-books">
-    {console.log(searchedBooks)}
+    {searchedBooks.map(book => (
+     <h1>{book.title}</h1>
+    ))}
             <div className="search-books-bar">
               <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
               <div className="search-books-input-wrapper">
